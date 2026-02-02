8a50d0f8d29e5d22de1ca1180eeca2b1e680977a
Just Pedro <13648996+juxtapix@users.noreply.github.com>
2026-02-02 18:59:06 -0500

    New Eleven Labs Example

---
+++ examples/02_STT/node/stt_labs.js
@@ -0,0 +1,14 @@
+//Example STT - "watson"
+
+var exec = require('child_process').execSync;
+
+function listen(something){
+  var api_key = "API-KEY";
+  var url = "https://api.elevenlabs.io/v1/speech-to-text";
+  var data = "@" + something;
+  command = `curl -X POST ${url} -H "xi-api-key: ${api_key}" -H "content-type: multipart/form-data" -F model_id=scribe_v1 -F file=${data} --output scribe.txt`
+  output = exec(command, function(error, stdout, stderr){console.log('stdout: ' + stdout);});
+  return output.toString().trim();
+}
+
+console.log(listen("_audio/audio-file.flac"));
